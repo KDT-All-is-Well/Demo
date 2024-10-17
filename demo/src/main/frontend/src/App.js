0b9f1a221ee7b73cb1c65a92daf55b78b1a9f7c9
Lumi <87791632+lumi-p@users.noreply.github.com>
2024-10-18 00:04:33 +0900

    feat. 전역 스타일, 테마 추가

--- demo/src/main/frontend/src/App.js
+++ demo/src/main/frontend/src/App.js
@@ -1,11 +1,25 @@
 import { Outlet } from 'react-router-dom';
+import { ThemeProvider, createGlobalStyle, styled } from 'styled-components';
+import theme from './styles/theme';
 
+
+const GlobalStyle = createGlobalStyle`
+    body {
+      min-width: 360px;
+      max-width: 1280px;
+      min-height: 740px;
+      margin: 0 auto;
+    }
+`;
 
 function App() {
     return (
-        <>
-            <Outlet />
-        </>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <>
+                <Outlet />
+            </>
+        </ThemeProvider>
     );
 }
 
---
+++ demo/src/main/frontend/src/styles/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#302723',
+        },
+        secondary: {
+            main: '#FAE100',
+        },
+        text: {
+            main: '#464646',
+        },
+    }
+});
+
+export default theme;
